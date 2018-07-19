26fbcd9b79fb7a8c453f94c2f6ed4ba8c1fdeb33
conan <ligangtang@dafy.com>
2018-07-19 18:30:41 +0800

    fix arcslidehelper bug

--- app/src/main/java/com/daydayup/mydemo/util/ArcSlideHelper.java
+++ app/src/main/java/com/daydayup/mydemo/util/ArcSlideHelper.java
@@ -1,11 +1,17 @@
 package com.daydayup.mydemo.util;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.FloatRange;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
 import android.view.View;
 import android.view.ViewParent;
 import android.widget.Scroller;
+
+import com.daydayup.mydemo.consts.Consts;
 
 /**
  * Created by conan on 2018/7/18.
@@ -15,14 +21,28 @@ import android.widget.Scroller;
 public class ArcSlideHelper {
 
     private VelocityTracker mVelocityTracker;
-    private int mPivotX;
-    private int mPivotY;
+    private int mPivotX,mPivotY;
     private OnSlidingListener mListener;
     private Scroller mScroller;
-    private float mStartX;
-    private float mStartY;
+    private float mStartX,mStartY;
     private boolean mIsSelfSliding;
     private boolean mIsInertialSlidingEnable;
+    private boolean mIsClockwiseScrolling;
+    private boolean mIsRecycled;
+
+    private Handler mHandler;
+    private boolean mIsShouldBeGetY;
+    private float mLastScrollOffset;
+    private float mScrollAvailabilityRatio;
+
+    /**
+     * VelocityTracker的惯性滚动利用率
+     * 数值越大，惯性滚动的动画时间越长
+     * @param ratio 滚动利用率(范围 0-1)
+     */
+    public void setScrollAvailabilityRatio(@FloatRange(from = 0,to = 1) float ratio){
+        mScrollAvailabilityRatio = ratio;
+    }
 
     private ArcSlideHelper(Context context, int pivotX, int pivotY, OnSlidingListener onSlidingListener) {
         this.mPivotX = pivotX;
@@ -30,6 +50,8 @@ public class ArcSlideHelper {
         this.mListener = onSlidingListener;
         this.mScroller = new Scroller(context);
         mVelocityTracker = VelocityTracker.obtain();
+        mScrollAvailabilityRatio = 0.3f;
+        mHandler = new InertialSlidingHandler(this);
     }
 
     public static ArcSlideHelper create(View targetView, OnSlidingListener onSlidingListener) {
@@ -48,6 +70,22 @@ public class ArcSlideHelper {
         return new ArcSlideHelper(targetView.getContext(), width + absoluteX, height + absoluteY, onSlidingListener);
     }
 
+    /**
+     * 设置自身滑动
+     * @param isSelfSliding 是否view自身滑动
+     */
+    public void setSelfSliding(boolean isSelfSliding){
+        mIsSelfSliding = isSelfSliding;
+    }
+
+    /**
+     * 设置惯性滑动
+     * @param isInertialSlidingEnable 是否要惯性滑动
+     */
+    public void setInertialSlidingEnable(boolean isInertialSlidingEnable){
+        mIsInertialSlidingEnable = isInertialSlidingEnable;
+    }
+
     private static int getAbsoluteX(View view) {
         float x = view.getX();
         ViewParent parent = view.getParent();
@@ -61,7 +99,7 @@ public class ArcSlideHelper {
         float y = view.getY();
         ViewParent parent = view.getParent();
         if (parent != null && parent instanceof View){
-            y += getAbsoluteX((View) parent);
+            y += getAbsoluteY((View) parent);
         }
         return (int) y;
     }
@@ -101,22 +139,141 @@ public class ArcSlideHelper {
         mStartY = y;
     }
 
-    private void checkIsRecycled() {
-
-    }
-
-    private void handleActionMove(float x, float y) {
-        float lineA = (float) Math.sqrt(Math.pow(Math.abs(mStartX - mPivotX), 2) + Math.pow(Math.abs(mStartY - mPivotY), 2));
-        float lineB = (float) Math.sqrt(Math.pow(Math.abs(x- mPivotX),2) + Math.pow(Math.abs(y - mPivotY),2));
-        float lineC = (float) Math.sqrt(Math.pow(Math.abs(x- mStartX),2) + Math.pow(Math.abs(y - mStartY),2));
-        double degrees = Math.toDegrees(Math.acos((Math.pow(lineA, 2) + Math.pow(lineB, 2) + Math.pow(lineC, 2)) / 2 * lineA * lineB));
-        float angle = fixAngle(degrees);
-        if (!Float.isNaN(angle)){
-            // TODO: 2018/7/18 滑动回调事件
-//            mListener.onSliding(mIsInertialSlidingEnable == );
+    /**
+     * 打断动画
+     */
+    public void abortAnimatinon(){
+        checkIsRecycled();
+        if (!mScroller.isFinished()){
+            mScroller.abortAnimation();
         }
     }
 
+    private void checkIsRecycled() {
+        if (mIsRecycled){
+            throw new RuntimeException("ArcSlideHelper is already recycled");
+        }
+    }
+
+    /**
+     * 更新当前手指触摸的坐标
+     * 在ViewGroup中的onInterceptTouchEvent中使用
+     * @param event 触摸事件
+     */
+    public void updateMovement(MotionEvent event){
+        checkIsRecycled();
+        if (event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_MOVE){
+            if (mIsSelfSliding){
+                mStartX = event.getRawX();
+                mStartY = event.getRawY();
+            }else {
+                mStartX = event.getX();
+                mStartY = event.getY();
+            }
+        }
+    }
+
+    /**
+     * 更新圆心坐标X
+     * @param pivotX 新的圆心坐标x
+     */
+    public void updatePivotX(int pivotX){
+        checkIsRecycled();
+        mPivotX = pivotX;
+    }
+
+    /**
+     * 更新圆心坐标Y
+     * @param pivotY 新的圆心坐标Y
+     */
+    public void updatePivotY(int pivotY){
+        checkIsRecycled();
+        mPivotY = pivotY;
+    }
+
+    /**
+     * 释放资源
+     */
+    public void release(){
+        checkIsRecycled();
+        mScroller = null;
+        mVelocityTracker.recycle();
+        mVelocityTracker = null;
+        mListener = null;
+        mHandler = null;
+        mIsRecycled = true;
+    }
+
+    private void handleActionMove(float x, float y) {
+//        float l, t, r, b;
+//        if (mStartX > x) {
+//            r = mStartX;
+//            l = x;
+//        } else {
+//            r = x;
+//            l = mStartX;
+//        }
+//        if (mStartY > y) {
+//            b = mStartY;
+//            t = y;
+//        } else {
+//            b = y;
+//            t = mStartY;
+//        }
+//        float pA1 = Math.abs(mStartX - mPivotX);
+//        float pA2 = Math.abs(mStartY - mPivotY);
+//        float pB1 = Math.abs(x - mPivotX);
+//        float pB2 = Math.abs(y - mPivotY);
+//        float hypotenuse = (float) Math.sqrt(Math.pow(r - l, 2) + Math.pow(b - t, 2));
+//        float lineA = (float) Math.sqrt(Math.pow(pA1, 2) + Math.pow(pA2, 2));
+//        float lineB = (float) Math.sqrt(Math.pow(pB1, 2) + Math.pow(pB2, 2));
+//        if (hypotenuse > 0 && lineA > 0 && lineB > 0) {
+//            float angle = fixAngle((float) Math.toDegrees(Math.acos((Math.pow(lineA, 2) + Math.pow(lineB, 2) - Math.pow(hypotenuse, 2)) / (2 * lineA * lineB))));
+//            if (!Float.isNaN(angle)) {
+//                mListener.onSliding((mIsClockwiseScrolling = isClockwise(x, y)) ? angle : -angle);
+//            }
+//        }
+
+        float lineA = (float) Math.sqrt(Math.pow(Math.abs(mStartX - mPivotX), 2) + Math.pow(Math.abs(mStartY - mPivotY), 2));
+        float lineB = (float) Math.sqrt(Math.pow(Math.abs(x- mPivotX),2) + Math.pow(Math.abs(y - mPivotY),2));
+        float lineC = (float) Math.sqrt(Math.pow(Math.abs(x- mStartX),2) + Math.pow(Math.abs(y - mStartY),2));
+
+        if (lineA > 0 && lineB > 0 && lineC > 0){
+            double degrees = Math.toDegrees(Math.acos((Math.pow(lineA, 2) + Math.pow(lineB, 2) - Math.pow(lineC, 2)) / (2 * lineA * lineB)));
+            float angle = fixAngle(degrees);
+            Log.v(Consts.LOG_TAG,this.getClass().getSimpleName() + "---angle = " + angle);
+            if (!Float.isNaN(angle)){
+                // 滑动回调事件
+                mIsClockwiseScrolling = isClockwise(x,y);
+                mListener.onSliding(mIsClockwiseScrolling ? angle : -angle);
+            }
+        }
+    }
+
+    /**
+     * 判断是否是顺时针方向
+     * @param x 当前x位置
+     * @param y 当前y位置
+     * @return  是否是顺时针
+     */
+    private boolean isClockwise(float x, float y) {
+        float deltaX = Math.abs(x - mStartX);
+        float deltaY = Math.abs(y - mStartY);
+        mIsShouldBeGetY = deltaY > deltaX;
+        if (deltaX < deltaY){
+            //竖向滑动
+            return x < mPivotX != y > mStartY;
+        }else {
+            //横向滑动
+            return y < mPivotY == x > mStartX;
+        }
+    }
+
+    /**
+     * 调整角度，使其在0~360之间
+     * @param degrees
+     * @return
+     */
     private float fixAngle(double degrees) {
         if (degrees < 0){
             degrees += 360;
@@ -127,12 +284,61 @@ public class ArcSlideHelper {
         return (float) degrees;
     }
 
+    /**
+     * 开始惯性滑动
+     */
     private void startFling() {
+        mHandler.sendEmptyMessage(0);
+    }
+
+    /**
+     * 处理惯性滚动
+     */
+    private void computeInertialSliding() {
+        checkIsRecycled();
+        if (mScroller.computeScrollOffset()){
+            float y = (mIsShouldBeGetY ? mScroller.getCurrY() : mScroller.getCurrX()) * mScrollAvailabilityRatio;
+            if (mLastScrollOffset != 0){
+                float offset = fixAngle(Math.abs(y - mLastScrollOffset));
+                mListener.onSliding(mIsClockwiseScrolling ? offset : -offset);
+            }
+            mLastScrollOffset = y;
+            startFling();
+        }else if (mScroller.isFinished()){
+            mLastScrollOffset = 0;
+        }
+    }
+
+    /**
+     * 惯性滚动分发器
+     */
+    private static class InertialSlidingHandler extends Handler{
+
+        ArcSlideHelper mHelper;
+
+        InertialSlidingHandler(ArcSlideHelper arcSlideHelper){
+            mHelper = arcSlideHelper;
+        }
+        @Override
+        public void handleMessage(Message msg) {
+            mHelper.computeInertialSliding();
+        }
 
     }
 
-    private interface OnSlidingListener{
+    /**
+     * 开始滑动监听器
+     */
+    public interface OnSlidingListener{
 
         void onSliding(float angle);
+    }
+
+    /**
+     * 滑动结束监听
+     */
+    public interface OnSlideFinishListener{
+
+        void onSlideFinished();
     }
 }
--- app/src/main/java/com/daydayup/mydemo/view/FanLayout.java
+++ app/src/main/java/com/daydayup/mydemo/view/FanLayout.java
@@ -2,23 +2,24 @@ package com.daydayup.mydemo.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.daydayup.mydemo.consts.Consts;
-
-import java.util.Locale;
+import com.daydayup.mydemo.util.ArcSlideHelper;
+import com.wuyr.ArcSlidingHelper;
 
 /**
  * Created by conan on 2018/7/18.
  * © 2017 Dafy Inc All Rights Reserved
  */
 
-public class FanLayout extends ViewGroup {
+public class FanLayout extends ViewGroup implements ArcSlidingHelper.OnSlidingListener, ArcSlideHelper.OnSlidingListener {
 
     private int mPivotX;
     private int mPivotY;
+    private ArcSlideHelper mArcSlideHelper;
+//    private ArcSlidingHelper mArcSlidingHelper;
 
     public FanLayout(Context context) {
         this(context, null);
@@ -36,24 +37,18 @@ public class FanLayout extends ViewGroup {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         measureChildren(widthMeasureSpec,heightMeasureSpec);
         setMeasuredDimension(widthMeasureSpec,heightMeasureSpec);
-        String.format(Locale.getDefault(),"","");
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         int childCount = getChildCount();
-        Log.v(Consts.LOG_TAG,this.getClass().getSimpleName() + "---childCount = " + childCount);
         float angle = 360f / childCount;
         mPivotX = getWidth() / 2;
         mPivotY = getHeight() / 2;
-        Log.v(Consts.LOG_TAG,this.getClass().getSimpleName() + "---mPivotX = " + mPivotX);
-        Log.v(Consts.LOG_TAG,this.getClass().getSimpleName() + "---mPivotY = " + mPivotY);
         for (int i = 0; i < childCount;i ++){
             View child = getChildAt(i);
             int layoutWidth = child.getMeasuredWidth();
             int layoutHeight = child.getMeasuredHeight() / 2;
-            Log.v(Consts.LOG_TAG,this.getClass().getSimpleName() + "---layoutWidth = " + layoutWidth);
-            Log.v(Consts.LOG_TAG,this.getClass().getSimpleName() + "---layoutHeight = " + layoutHeight);
 
             child.layout(mPivotX,mPivotY - layoutHeight,mPivotX + layoutWidth,mPivotY + layoutHeight);
             child.setPivotX(0);
@@ -62,9 +57,48 @@ public class FanLayout extends ViewGroup {
         }
     }
 
+    /**
+     * 在onSizeChanged方法中初始化ArcSlideHelper的原因是：
+     * 这个方法回调时，getWidth()和getHeight()方法以及获取到了正确的宽高
+     */
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
+        if (mArcSlideHelper == null){
+//            mArcSlidingHelper = ArcSlidingHelper.create(this,this);
+//            mArcSlidingHelper.enableInertialSliding(true);
+            mArcSlideHelper = ArcSlideHelper.create(this,this);
+            mArcSlideHelper.setInertialSlidingEnable(true);
+            mArcSlideHelper.setSelfSliding(true);
+        }else {
+//            mArcSlidingHelper.updatePivotX(w / 2);
+//            mArcSlidingHelper.updatePivotY(h / 2);
+            mArcSlideHelper.updatePivotX(w / 2);
+            mArcSlideHelper.updatePivotY(h / 2);
+        }
+    }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+//        mArcSlidingHelper.handleMovement(event);
+        mArcSlideHelper.handleMovement(event);
+        return true;
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        if (mArcSlideHelper != null){
+            mArcSlideHelper.release();
+            mArcSlideHelper = null;
+        }
+    }
+
+    @Override
+    public void onSliding(float angle) {
+        for (int i = 0;i < getChildCount();i ++){
+            View view = getChildAt(i);
+            view.setRotation(view.getRotation() + angle);
+        }
     }
 }
